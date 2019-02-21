9186c5af885f00e6a7ef3f611cf021ebe42fe4c8
lygadanylo <31656731+lygadanylo@users.noreply.github.com>
2019-02-21 21:01:30 +0200

    Добавьте файлы проекта.

---
+++ TextReader/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TextReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aNSIToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.uNICODEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unicodeToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.uTF8ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleaneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unicodeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.uTF8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 411);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.cleaneToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.formatToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem1
+            // 
+            this.formatToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aNSIToolStripMenuItem1,
+            this.uNICODEToolStripMenuItem});
+            this.formatToolStripMenuItem1.Name = "formatToolStripMenuItem1";
+            this.formatToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.formatToolStripMenuItem1.Text = "Format";
+            // 
+            // aNSIToolStripMenuItem1
+            // 
+            this.aNSIToolStripMenuItem1.Name = "aNSIToolStripMenuItem1";
+            this.aNSIToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.aNSIToolStripMenuItem1.Text = "ANSI";
+            this.aNSIToolStripMenuItem1.Click += new System.EventHandler(this.aNSIToolStripMenuItem1_Click);
+            // 
+            // uNICODEToolStripMenuItem
+            // 
+            this.uNICODEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unicodeToolStripMenuItem2,
+            this.uTF8ToolStripMenuItem1});
+            this.uNICODEToolStripMenuItem.Name = "uNICODEToolStripMenuItem";
+            this.uNICODEToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uNICODEToolStripMenuItem.Text = "UNICODE";
+            // 
+            // unicodeToolStripMenuItem2
+            // 
+            this.unicodeToolStripMenuItem2.Name = "unicodeToolStripMenuItem2";
+            this.unicodeToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.unicodeToolStripMenuItem2.Text = "Unicode";
+            this.unicodeToolStripMenuItem2.Click += new System.EventHandler(this.unicodeToolStripMenuItem2_Click);
+            // 
+            // uTF8ToolStripMenuItem1
+            // 
+            this.uTF8ToolStripMenuItem1.Name = "uTF8ToolStripMenuItem1";
+            this.uTF8ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.uTF8ToolStripMenuItem1.Text = "UTF8";
+            this.uTF8ToolStripMenuItem1.Click += new System.EventHandler(this.uTF8ToolStripMenuItem1_Click);
+            // 
+            // cleaneToolStripMenuItem
+            // 
+            this.cleaneToolStripMenuItem.Name = "cleaneToolStripMenuItem";
+            this.cleaneToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.cleaneToolStripMenuItem.Text = "Cleane";
+            this.cleaneToolStripMenuItem.Click += new System.EventHandler(this.cleaneToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            //
+            // uTF8ToolStripMenuItem
+            // 
+            this.uTF8ToolStripMenuItem.Name = "uTF8ToolStripMenuItem";
+            this.uTF8ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleaneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aNSIToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem unicodeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem uTF8ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uNICODEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unicodeToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem uTF8ToolStripMenuItem1;
+    }
+}
+
---
+++ TextReader/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Danylo Lyga");
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFile.FileName);
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.DefaultExt = "*.txt";
+            saveFile.Filter = "Text files|*.txt";
+            if(saveFile.ShowDialog()== DialogResult.OK&&saveFile.FileName.Length>0)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile.FileName,true))
+                {
+                    sw.WriteLine(richTextBox1.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cleaneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void aNSIToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Read read = new Read();
+            read.ReadFormatAnsi();
+            richTextBox1.Text = read.text;    
+            
+        }
+        class Read
+        {
+            public string text { get; set; }
+            public  void ReadFormatUnicode()
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(openFile.FileName, Encoding.Unicode))
+                        {
+                            text = sr.ReadToEnd();
+                            sr.Close();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            public void ReadFormatAnsi()
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(openFile.FileName, Encoding.Default))
+                        {
+                            text = sr.ReadToEnd();
+                            sr.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+        private void uTF8ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Read read = new Read();
+            read.ReadFormatUnicode();
+            Encoding unicode = Encoding.Unicode;
+            byte[] unicodeBytes = unicode.GetBytes(read.text);
+            String str2 = Encoding.UTF8.GetString(unicodeBytes);
+            richTextBox1.Text = str2;
+        }
+
+        private void unicodeToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Read read = new Read();
+            read.ReadFormatUnicode();
+            Encoding unicode = Encoding.Unicode;
+            byte[] unicodeBytes = unicode.GetBytes(read.text);
+            String str1 = Encoding.Unicode.GetString(unicodeBytes);
+            richTextBox1.Text = str1;
+        }
+    }
+}
